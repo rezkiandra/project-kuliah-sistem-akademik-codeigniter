afc1d971c410f18c246a43af49aead6dccc25c8a
unknown <rezkiandra4123@gmail.com>
2023-07-23 14:28:23 +0700

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./applications/views/**/*.{php}"
+    ],
+    theme: {
+        extend: {
+            spacing: {
+                // 'fluxo': '52rem',
+            }
+        },
+    },
+    plugins: [
+
+    ],
+}
